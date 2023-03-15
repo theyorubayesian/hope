43fe478b04209a0413e1512f9c466301a5501fa3
theyorubayesian <akin.o.oladipo@gmail.com>
2023-03-15 14:42:01 -0400

    allow tweets and users as graph nodes

--- src/hope/twitter/graph.py
+++ src/hope/twitter/graph.py
@@ -1,4 +1,3 @@
-from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
@@ -16,25 +15,33 @@ Edge = TypedDict('Edge', {"source": str, "type": str, "target": str, "weight": f
 
 
 # flake8: noqa: CCR001
-def get_graphing_info(tweet_item: TweetItem) -> Tuple[Optional[List[Node]], Optional[List[Edge]]]:
+def get_graphing_info(
+        tweet_item: TweetItem, 
+        tweets_as_nodes: bool = False
+    ) -> Tuple[Optional[List[Node]], Optional[List[Edge]]]:
     """
-    Get graphing info for Gephi.
+    Get graphing info for Gephi or d3.js
 
-     - Users are collected as nodes.
+     - Users/Tweets are collected as nodes.
      - Engagements (retweets, quotes and replies) are collected as edges.
 
     @param tweet_item: TweetItem to get graphing data from
 
     @return: nodes, edge if `tweet_item` contains an engagement else None, None
     """
+    _ENGAGEMENT_TYPE = f"{'Tweet' if tweets_as_nodes else 'User'}-{ENGAGEMENT_TYPE}"
+
     if any([tweet_item.is_quote, tweet_item.is_reply, tweet_item.is_retweet]):
-        node_ids = set()
         node_info: List[Node] = []
         edge_info: List[Edge] = []
 
-        node_ids.add(tweet_item.user_id)
         node_info.append(
             {
+                "id": tweet_item.tweet_id,
+                "tweet": tweet_item.text,
+                "user_id": tweet_item.user_id,
+                "timestamp": tweet_item.timestamp['$date']
+            } if tweets_as_nodes else {
                 "id": tweet_item.user_id,
                 "name": tweet_item.user_screen_name,
                 "createdat": tweet_item.user_created_at,
@@ -44,59 +51,90 @@ def get_graphing_info(tweet_item: TweetItem) -> Tuple[Optional[List[Node]], Opti
         if tweet_item.is_quote:
             edge_info.append(
                 {
-                    "source": tweet_item.user_id,
-                    "type": ENGAGEMENT_TYPE,
-                    "target": tweet_item.qt_user_id,
+                    "source": tweet_item.tweet_id if tweets_as_nodes else tweet_item.user_id,
+                    "type": _ENGAGEMENT_TYPE,
+                    "action": "quote",
+                    "target": tweet_item.qt_id if tweets_as_nodes else tweet_item.qt_user_id,
                     "weight": DEFAULT_ENGAGEMENT_WEIGHTS["quote"],
                 }
             )
 
-            if tweet_item.qt_user_id != node_info[0]["id"]:
+            if tweets_as_nodes:
+                node_info.append(
+                    {
+                        "id": tweet_item.qt_id,
+                        "tweet": "",    # TODO: Backfill
+                        "user_id": tweet_item.qt_user_id,
+                        "timestamp":""  # TODO: Backfill
+                    }  
+                )
+            elif tweet_item.qt_user_id != node_info[0]["id"]:
                 node_info.append(
                     {
                         "id": tweet_item.qt_user_id,
                         "name": tweet_item.qt_user_screen_name,
-                        "createdat": tweet_item.qt_user_created_at,
+                        "createdat": tweet_item.qt_user_created_at['$date'],
                     }
                 )
 
         if tweet_item.is_retweet:
             edge_info.append(
                 {
-                    "source": tweet_item.user_id,
-                    "type": ENGAGEMENT_TYPE,
-                    "target": tweet_item.rt_user_id,
+                    "source": tweet_item.tweet_id if tweets_as_nodes else tweet_item.user_id,
+                    "type": _ENGAGEMENT_TYPE,
+                    "action": "retweet",
+                    "target": tweet_item.rt_id if tweets_as_nodes else tweet_item.rt_user_id,
                     "weight": DEFAULT_ENGAGEMENT_WEIGHTS["retweet"],
                 }
             )
 
-            if tweet_item.rt_user_id != node_info[0]["id"]:
+            if tweets_as_nodes:
+                node_info.append(
+                    {
+                        "id": tweet_item.rt_id,
+                        "tweet": "",    # TODO: Backfill
+                        "user_id": tweet_item.rt_user_id,
+                        "timestamp":""  # TODO: Backfill
+                    }  
+                )
+            elif tweet_item.rt_user_id != node_info[0]["id"]:
                 node_info.append(
                     {
                         "id": tweet_item.rt_user_id,
                         "name": tweet_item.rt_user_screen_name,
-                        "createdat": tweet_item.rt_user_created_at,
+                        "createdat": tweet_item.rt_user_created_at['$date'],
                     }
                 )
+            
         if tweet_item.is_reply:
             edge_info.append(
                 {
-                    "source": tweet_item.user_id,
-                    "type": ENGAGEMENT_TYPE,
-                    "target": tweet_item.in_reply_to_user_id,
+                    "source": tweet_item.tweet_id if tweets_as_nodes else tweet_item.user_id,
+                    "action": "reply",
+                    "type": _ENGAGEMENT_TYPE,
+                    "target": tweet_item.in_reply_to_tweet_id if tweets_as_nodes else tweet_item.in_reply_to_user_id,
                     "weight": DEFAULT_ENGAGEMENT_WEIGHTS["reply"],
                 }
             )
 
-            if tweet_item.in_reply_to_user_id != node_info[0]["id"]:
+            if tweets_as_nodes:
+                node_info.append(
+                    {
+                        "id": tweet_item.in_reply_to_tweet_id,
+                        "tweet": "",    # TODO: Backfill
+                        "user_id": tweet_item.in_reply_to_user_id,
+                        "timestamp":""  # TODO: Backfill
+                    }  
+                )
+            elif tweet_item.in_reply_to_user_id != node_info[0]["id"]:
                 node_info.append(
                     {
                         "id": tweet_item.in_reply_to_user_id,
                         "name": tweet_item.in_reply_to_user_screen_name,
+                        "createdat": "" # TODO: Backfill
                     }
                 )
 
-        # TODO: Get weight for like and retweet
         return node_info, edge_info
 
     # If not an engagement, we return None for nodes & edge
